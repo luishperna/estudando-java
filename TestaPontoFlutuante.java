15195cc5212580d3190637e650822bc67bc483e7
Luís <luishperna@gmail.com>
2022-09-27 23:17:53 -0300

    Declarando e atribuindo variáveis

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,31 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		/* Declarando a variável salario como o tipo double */
+		double salario;
+		salario = 1250.70;
+		
+		System.out.println("Meu salário é " + salario);
+		
+		/* Declarando a variável idade como o tipo double e atribuindo um valor */
+		double idade = 19;
+		System.out.println("Minha idade é " + idade);
+		
+		/* Calculando diretamente na variável do tipo double */
+		double divisao = 3.14 / 2;
+		System.out.println("O resultado da divisão é " + divisao);
+		
+		/* Obs.: O lado direito é executado primeiro e, posteriormente, o direito, ou seja, o cálculo
+		   entre inteiros é executado primeiro e depois esse valor é passado para o tipo inteiro */
+		int outraDivisao = 5 / 2;
+		System.out.println("O resultado da parte inteira da divisão é " + outraDivisao);
+		
+		/* Obs.: Nesse caso o cálculo está entre valores do tipo double pelo 5.0, logo o resultado será
+		   um valor do tipo double */
+		double novaTentativa = 5.0 / 2;
+		System.out.println("O resultado da divisão agora é " + novaTentativa);
+				
+	}
+}
---
+++ TestaVariaveis.java
@@ -0,0 +1,29 @@
+
+/* Nome da classe em CamelCase */
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá, novo teste!");
+		
+		/* Declarando a variável idade como um valor do tipo inteiro */
+		int idade;
+		
+		/* Atribuindo um valor para a variável idade */
+		idade = 19;
+		
+		/* Mostrando a variável idade no console */
+		System.out.println(idade);
+		
+		/* Trabalhando com operadores aritméticos na variável idade */
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = 7 * 5 + 2;
+		
+		System.out.println(idade);
+		
+		/* Concatenando a variável idade em uma string*/
+		System.out.println("A idade é: " + idade + ", parabéns!");
+	}
+}
