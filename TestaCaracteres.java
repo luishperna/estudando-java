add3dba6e16297714c96eb30b43bdeaf6b69f49e
Luís <luishperna@gmail.com>
2022-10-06 23:37:04 -0300

    Adicionando conteúdo das aulas

---
+++ TestaCaracteres.java
@@ -0,0 +1,22 @@
+
+public class TestaCaracteres {
+
+		public static void main(String[] args) {
+			//O tipo char aceita apenas um carácter (no fundo é um número)
+			char letra = 'a';
+			System.out.println(letra);
+			
+			char valor = 65;
+			System.out.println(valor);
+			
+			valor = (char) (valor + 1);
+			System.out.println(valor);
+			
+			String palavra = "alura cursos online de tecnologia";
+			System.out.println(palavra);
+			
+			palavra = palavra + 2020;
+			System.out.println(palavra);
+
+		}			
+}
---
+++ TestaValores.java
@@ -0,0 +1,14 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+
+		// Não aplica o valor após a atribuição
+		primeiro = 10;
+
+		System.out.println(segundo);
+	}
+}
